6ff4d725021cd272f8800cbb0f16fd61529a2e3a
Kindaglia <matteodrago007@gmail.com>
2025-07-24 11:41:08 +0200

    Add dotenv dependency and implement environment variable loading

--- weathermap-cargo-rust-only/src/main.rs
+++ weathermap-cargo-rust-only/src/main.rs
@@ -1,3 +1,12 @@
+use dotenv::dotenv;
+use std::env;
+
+fn get_value_from_env() {
+    dotenv().ok(); // Load environment variables from a .env file, if present
+    let api_key = env::var("WEATHER_API_KEY").expect("Variable not found");
+    println!("API Key: {}", api_key);
+}
+
 fn main() {
-    println!("Hello, world!");
+    get_value_from_env();
 }
